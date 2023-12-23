ae6046c3a7a6ec978b63e184c92027da2bc266be
Moran Thaler <thaler.moran@gmail.com>
2023-12-23 17:42:43 +0100

    create the file using vite, setup prettier, eslint, sass

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div className="app">
+      <h1>Hallo World!</h1>
+    </div>
+  );
+};
+export default App;
